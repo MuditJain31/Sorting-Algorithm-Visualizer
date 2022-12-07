d78044e3152ec4b87fcaf8cb533281aad93fd5ad
MuditJain31 <muditjain921@gmail.com>
2022-12-07 15:27:45 +0530

    Graph displaying

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,36 @@
 
-const buttons = document.querySelectorAll('div.sortingAlgos button');
-console.log(buttons)
-buttons.addEventListener("click",output);
 
-function output(){
-    let val = document.getElementById("arraySize").value;
-    console.log(val);
+function createArray(divs,length){
+    console.log('inside create array');
+    let cont = document.querySelector('div.showArea');
+    let size = document.getElementById('arraySize').value;
+    console.log(size);
+    for(let i=0;i<size;i++){
+        let temp = Math.floor((Math.random()*size) + 1);
+        length.push(temp);
+        let elem = document.createElement('div');
+        elem.style.marginLeft=`${0.5}%`;
+        elem.style.height=`${temp/size * 95}%`;
+        elem.style.width=`${100/size - 0.5}%`;
+        elem.style.backgroundColor='red';
+        divs.push(elem);
+        cont.appendChild(elem);
+    }
+    console.log(divs,length);
 }
+
+function main(){
+    const buttons = document.querySelectorAll('div.sortingAlgos button');
+    console.log(buttons);
+    let divs=new Array();
+    let length=new Array();
+    buttons.forEach(element => {
+        element.addEventListener('click',()=>{
+            console.log("inside event listener");
+            createArray(divs,length);
+        })
+    });
+    console.log(divs,length);
+
+}
+main();
